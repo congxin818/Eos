bb4c513b3be2171bb205cd7042bde913447cfb38
Yuzhi <1139018783@qq.com>
2017-10-17 16:48:17 +0800

    解决administrator表不能不能添加中文

--- models/administrator.js
+++ models/administrator.js
@@ -8,7 +8,7 @@ var Administrator = sequelize.define('administrator' , {
         primaryKey: true
     },
     username: { //名字
-        type: Sequelize.STRING(50)
+        type: Sequelize.STRING(50) charset utf8 collate utf8_general_ci
     },
     password: { //密码
         type: Sequelize.STRING(100)
