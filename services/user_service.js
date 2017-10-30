cfa823369527bced2b08a5c9d121ca0193e329cd
Yuzhi <1139018783@qq.com>
2017-10-30 09:53:23 +0800

    test_userAddOne

--- services/user_service.js
+++ services/user_service.js
@@ -285,12 +285,10 @@ exports.findAndCount = findAndCount;
         const jsonString = req.body.validArea;
     //console.log('validArea->'+jsonString);
     //console.log('menuids_str->'+menuids_str);
-    if (jsonString != null || jsonString != '') {
         let groupIds = await stringUtil.getIds(jsonString , '');
         let factoryIds = await stringUtil.getIds(jsonString , 'f');
         let workshopIds = await stringUtil.getIds(jsonString , 'w');
         let linebodyIds = await stringUtil.getIds(jsonString , 'l');
-    }
     //console.log('yuzhizhe01->'+groupIds.length);
     // console.log('yuzhizhe02->'+factoryIds.length);
     // console.log('yuzhizhe03->'+workshopIds.length);
