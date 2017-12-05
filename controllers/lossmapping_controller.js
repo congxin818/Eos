c391e7a99bf358a4aa383850b54bb2b5debaf9b0
Yuzhi <1139018783@qq.com>
2017-12-05 15:52:43 +0800

    tests

--- controllers/lossmapping_controller.js
+++ controllers/lossmapping_controller.js
@@ -1,7 +1,7 @@
 /*
- auther:Android,
- NOTE:lossmapping表的controller层,
- time:20171108
+ 	auther:Android,
+ 	NOTE:lossmapping表的controller层,
+ 	time:20171108
  */
 
 var service = require('../services/user_service');
@@ -22,117 +22,6 @@ var dataSuccess = {
     msg: '请求成功',
     data:'fas'
 };
-
-
-Date.prototype.format = function(format) {
-  var o = {
-    "M+": this.getMonth() + 1,
-    "d+": this.getDate(),
-    "h+": this.getHours(),
-    "m+": this.getMinutes(),
-    "s+": this.getSeconds(),
-    "q+": Math.floor((this.getMonth() + 3) / 3),
-    "S": this.getMilliseconds()
-  }
-  if (/(y+)/.test(format)) {
-    format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp("(" + k + ")").test(format)) {
-      format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
-    }
-  }
-  return format;
-}
-/*
-	linebody级别为本次查询计算的最高级
- */
-async function selectLossMappingByLinebodyId(userId , linebodyId) {
-	if (linebodyId == undefined || linebodyId == null || linebodyId == ''
-		||userId == undefined || userId == null || userId == '') {
-		return ;
-	}
-	const user = await User.findById(userId);
-	const linebody = await Linebody.findById(linebodyId);
-	//console.log(JSON.stringify(linebody , null , 4));
-	//console.log(JSON.stringify(user , null , 4));
-	if (linebody == undefined || linebody == null || linebody == ''
-		||user == undefined || user == null || user == '') {
-		return ;
-	}
-
-	let  tier2 = await user.getUserKpitwolevs({'attributes': ['name', 'kpitwoid']});
-	const kpitwos = await linebody.getLinebodyKpitwolev({'attributes': ['name', 'kpitwoid' , 'id' , 'value']});
-	//console.log(JSON.stringify(tier2 , null , 4));
-	if (kpitwos == undefined || kpitwos == null || kpitwos == ''
-		||tier2 == undefined || tier2 == null || tier2 == '') {
-		return ;
-	}
-	let alldata = [];
-	for (var i = kpitwos.length - 1; i >= 0; i--) {
-		let kpitwo = {
-			name:kpitwos[i].name,
-			value:kpitwos[i].value
-		};
-		const kpitwoid = kpitwos[i].kpitwoid;
-		let orderStr;
-		for (var m = tier2.length - 1; m >= 0; m--) {
-			if (kpitwoid == tier2[m].kpitwoid) {
-				orderStr = tier2[m].userKpitwolev.sequence;
-			}
-		}
-		//console.log(JSON.stringify(orderStr , null , 4));
-		let array = {
-			title:kpitwos[i].name,
-			order:orderStr,
-			data: new Array (),
-			link: new Array ()
-		};
-		let datas = new Array ();
-		let links = new Array ();
-		datas.push(kpitwo);
-		const losscategorys = await kpitwos[i].getKpitwolevLosscategory({'attributes': ['name', 'lossid' , 'id' , 'value']});
-
-		for (var j = losscategorys.length - 1; j >= 0; j--) {
-			let losscategory = {
-				name:losscategorys[j].name,
-				value:losscategorys[j].value
-			};
-			let link = {
-				source:kpitwos[i].name,
-				target:losscategorys[j].name,
-				value:losscategorys[j].value
-			};
-			datas.push(losscategory);
-			links.push(link);
-			const losstier4s = await losscategorys[j].getLosscategoryLosstier4({'attributes': ['name',  'id' , 'value']});
-			for (var k = losstier4s.length - 1; k >= 0; k--) {
-				let losstier4 = {
-					name:losstier4s[k].name,
-					value:losstier4s[k].value
-				};
-				let link1 = {
-					source:losscategorys[j].name,
-					target:losstier4s[k].name,
-					value:losstier4s[k].value
-				};
-				datas.push(losstier4);
-				links.push(link1);
-			}
-			//console.log(JSON.stringify(losstier4s , null , 4));
-		}
-		array.data = datas;
-		array.link = links;
-		alldata.push(array);
-		alldata.sort ((a, b) => a.order - b.order)
-		// console.log ('/n/n/n/n/n/n')
-		// alldata.forEach (a => console.log (a.order))
-		//console.log(JSON.stringify(losscategorys , null , 4));
-	}
-	dataSuccess.data = alldata;
-	return dataSuccess;
-}
-exports.selectLossMappingByLinebodyId = selectLossMappingByLinebodyId;
 
 /*
 	根据线体ID查询Kpitwo的数据
@@ -316,9 +205,9 @@ async function selectLossMappingByLinebodyIds(userId , linebodyIds , startTime ,
 	//console.log('-=============');
  	const lossTier4Map = await computeLosstier4(allLosstier4 , weight_sum);
  	
- 	console.log(kpitwoMap);
- 	console.log(lossTier3Map);
- 	console.log(lossTier4Map);
+ 	// console.log(kpitwoMap);
+ 	// console.log(lossTier3Map);
+ 	// console.log(lossTier4Map);
 	for(var [key, value] of kpitwoMap) {
 		const kpitwo = await Kpitwolev.findById(key);
 		let pushkpitwo = {
