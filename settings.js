335e2750b315ab37405d97f0b465551eebe55632
Yuzhi <1139018783@qq.com>
2018-01-05 16:42:05 +0800

    test

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ mysql: {
           host: 'localhost',
           port: 3306,
           user: 'root',
-          password: 'mysql',
+          password: 'e009b3a4c1',
           database: 'db_node',
           charset:'utf8',
           collate:'utf8_general_ci'
